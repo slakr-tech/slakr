cf79a56e4e9c9e3e6560b9748945936d799b3d15
Sam Girshovich <sam.girshovich@gmail.com>
2020-12-19 23:53:48 -0500

    commit1

---
+++ public/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyBOUs6O3tU5aJL_GH4uPED8FszqjdtczTo",
+    authDomain: "social-app-de1b4.firebaseapp.com",
+    projectId: "social-app-de1b4",
+    storageBucket: "social-app-de1b4.appspot.com",
+    messagingSenderId: "152899851848",
+    appId: "1:152899851848:web:d149a25289968ac5aa2a7d",
+    measurementId: "G-DCY2HR4NV6"
+};
