f8746e4532a731caa6dd99973d2fa8ff02181361
Sam Girshovich <sam.girshovich@gmail.com>
2020-12-20 00:35:52 -0500

    basic user auth

--- public/app.js
+++ public/app.js
@@ -1,5 +1,26 @@
-import { firebaseConfig } from './firebase-config.js';
-
-firebase.initializeApp(firebaseConfig);
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+const userDetails = document.getElementById('userDetails');
 
 console.log(firebase);
+
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // signed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+    } else {
+        // not signed in
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
+    }
+});
--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,5 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyBOUs6O3tU5aJL_GH4uPED8FszqjdtczTo",
     authDomain: "social-app-de1b4.firebaseapp.com",
     projectId: "social-app-de1b4",
@@ -8,3 +8,5 @@ export const firebaseConfig = {
     appId: "1:152899851848:web:d149a25289968ac5aa2a7d",
     measurementId: "G-DCY2HR4NV6"
 };
+
+firebase.initializeApp(firebaseConfig);
