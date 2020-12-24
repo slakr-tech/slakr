cad325c97514ae9293dd744fa46b61018a6f0d17
Sam Girshovich <sam.girshovich@gmail.com>
2020-12-23 22:18:28 -0500

    added login.html and imported session

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 import database
 
 app = Flask(__name__)
