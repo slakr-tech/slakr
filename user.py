7962604bd7cf087aa100b3930159884a17b86229
samg11 <Sam.Girshovich@gmail.com>
2020-12-26 01:58:00 +0000

    removed email verification

--- user.py
+++ user.py
@@ -1,7 +1,3 @@
-import yagmail
-from random import randint
-import os
-
 class User:
     def __init__(self, username, first_name, last_name, email, email_confirmed):
         self.username        = username
@@ -9,17 +5,3 @@ class User:
         self.last_name       = last_name
         self.email           = email
         self.email_confirmed = email_confirmed
-
-    def send_confirmation_email(self):
-        yagmail.register(os.environ["chatreEmail"], os.environ["chatreEmailPassword"])
-        yag = yagmail.SMTP(os.environ["chatreEmail"])
-        with open('email.html', 'r') as f:
-            email_content = f.read()
-        yag.send(to = self.email,
-            subject = 'CHATRE email verification',
-            contents = email_content.format(
-                self.first_name,
-                self.email,
-                randint(100000, 999999)
-                )
-            )
